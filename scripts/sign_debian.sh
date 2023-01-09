7f5540cbc69af2a704457a5709eead365a1bf85b
Andrew Hamilton <hamilton@mbari.org>
2023-01-08 21:21:10 -0800

    Updated .deb

---
+++ scripts/sign_debian.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+apt-ftparchive release . > Release
+gpg --default-key "hamilton@mbari.org" -abs -o - Release > Release.gpg
+gpg --default-key "hamilton@mbari.org" --clearsign -o - Release > InRelease
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/ppa.gpg] https://hamilton8415.github.io/ppa ./" > my_list_file.list
